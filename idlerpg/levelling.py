08088e6acea25af6a4abbcc8af89f8accd653198
Elijah Newren <newren@palantir.com>
2015-02-20 10:43:21 -0800

    Put all stats into common dict with good defaults

--- idlerpg/levelling.py
+++ idlerpg/levelling.py
@@ -10,6 +10,7 @@
 #     come in giving me updated information.
 
 from datetime import datetime, timedelta
+from collections import defaultdict
 import math
 import re
 import subprocess
@@ -21,11 +22,11 @@ def convert_to_epoch(timestring):
 def convert_to_duration(days, hours, mins, secs):
   return 86400*int(days) + 3600*int(hours) + 60*int(mins) + int(secs)
 
-level = {}     # who_string -> level_number
-online = {}    # who_string -> boolean (well, False or True or None=unknown)
-timeleft = {}  # who_string -> seconds_left
-itemsum = {}   # who_string -> latest_battle_itemsum
-alignment = {} # who_string -> alignment
+def default_player():
+  return {'level':0, 'online':None, 'timeleft':0, 'itemsum':0,
+          'alignment':'neutral'}
+
+stats = defaultdict(default_player)
 player = {}  # ircnick -> who_string
 quest_started = None  # time_string or None
 quest_times = []
@@ -43,11 +44,6 @@ def handle_timeleft(m):
   mins = m.group('mins')
   secs = m.group('secs')
 
-  # If we've been told about time to next level, but don't know this
-  # person's level yet, just skip this info.
-  if who not in level:
-    return
-
   # Given that we were told the time to the next level and know when we
   # were told that, determine how much time currently remains until that
   # character reaches the next level
@@ -55,23 +51,22 @@ def handle_timeleft(m):
   post_delta = convert_to_duration(days, hours, mins, secs)
   now_delta = (post_epoch+post_delta)-now
 
-  timeleft[who] = now_delta
+  stats[who]['timeleft'] = now_delta
 
 def adjust_timeleft(who, post_epoch):
-  if who in timeleft and online[who]:
-    timeleft[who] += (now-post_epoch)
+  if stats[who]['online']:
+    stats[who]['timeleft'] += (now-post_epoch)
 
 def adjust_timeleft_percentage(who, post_epoch, percentage):
-  if who in timeleft:
-    timeremaining = timeleft[who] + (now-post_epoch)
-    timeleft[who] = (1-percentage/100.0)*timeremaining - (now-post_epoch)
+  then_diff = (now-post_epoch)
+  time_then_remaining = stats[who]['timeleft'] + then_diff
+  stats[who]['timeleft'] = (1-percentage/100.0)*time_then_remaining - then_diff
 
 def reward_questers(quest_end, wholist):
   questers = re.findall('[^, ]+', wholist)
   questers.remove('and')
   for who in questers:
-    if who in timeleft:
-      adjust_timeleft_percentage(who, quest_end, 25)
+    adjust_timeleft_percentage(who, quest_end, 25)
 
 with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
   for line in f:
@@ -99,9 +94,9 @@ with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
       quest_time_left = None
       questers = None
       next_quest = convert_to_epoch(end)+43200
-      for p in online:
-        if online[p] and p in level:
-          timeleft[p] += 15*1.14**level[p]
+      for p in stats:
+        if stats[p]['online']:
+          stats[p]['timeleft'] += 15*1.14**stats[p]['level']
       continue
     m = re.match(r"([\d-]{10} [\d:]{8}) <idlerpg>.*completed their journey", line)
     if m:
@@ -134,11 +129,9 @@ with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
     m = re.match(postdate_re+r"Welcome (?P<nick>.*)'s new player (?P<who>.*), the .*! "+nextlvl_re, line)
     if m:
       who = m.group('who')
-      level[who] = 0
-      itemsum[who] = 0
-      alignment[who] = 'neut'
-      online[who] = True
       player[m.group('nick')] = who
+      # Defaults for level, itemsum, alignment are fine
+      stats[who]['online'] = True
       handle_timeleft(m)
       continue
 
@@ -146,8 +139,8 @@ with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
     m = re.match(postdate_re+r"(?P<who>.*), the level .*, is now online from nickname (?P<nick>.*). "+nextlvl_re, line)
     if m:
       who = m.group('who')
-      online[who] = True
       player[m.group('nick')] = who
+      stats[who]['online'] = True
       handle_timeleft(m)
       continue
 
@@ -155,9 +148,7 @@ with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
     m = re.match(postdate_re+r"(?P<who>.*), the .*, has attained level (?P<level>\d+)! "+nextlvl_re, line)
     if m:
       who = m.group('who')
-      level[who] = int(m.group('level'))
-      if who not in alignment:
-        alignment[who] = 'neut'
+      stats[who]['level'] = int(m.group('level'))
       handle_timeleft(m)
       continue
 
@@ -168,14 +159,14 @@ with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
     #   other cases like Critical Strikes or light of their God or hand of God.
     m = re.match(postdate_re+r"(?P<who>.*) reaches "+nextlvl_re, line)
     if m:
-      online[m.group('who')] = True
+      stats[m.group('who')]['online'] = True
       handle_timeleft(m)
       continue
 
     # Y, the level W Z, is #U! Next level in...
     m = re.match(postdate_re+r"(?P<who>.*?), the level .*, is #\d+! "+nextlvl_re, line)
     if m:
-      if online.get(m.group('who'), False):
+      if stats[m.group('who')]['online']:
         handle_timeleft(m)
       continue
 
@@ -189,11 +180,11 @@ with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
       nick = m.group('nick')
       post_epoch = convert_to_epoch(m.group('postdate'))
       who = player.get(nick)
-      if who:
+      if who in stats:
         adjust_timeleft(who, post_epoch)
-        if who in timeleft and online[who]:
-          timeleft[who] += 20*1.14**level[who]
-        online[who] = False
+        if stats[who]['online']:
+          stats[who]['timeleft'] += 20*1.14**stats[who]['level']
+        stats[who]['online'] = False
       continue
 
     # I got disconnected somehow
@@ -203,9 +194,9 @@ with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
       timetuple = datetime.strptime(enddate, '%a %b %d %H:%M:%S %Y').timetuple()
       post_epoch = time.mktime(timetuple)
       final_log_break = post_epoch
-      for who in online:
+      for who in stats:
         adjust_timeleft(who, post_epoch)
-        online[who] = None
+        stats[who]['online'] = None
       continue
 
     #
@@ -216,8 +207,10 @@ with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
     m = re.match(postdate_re+r"(?P<attacker>.*) \[\d+/(?P<attacker_sum>\d+)\] has (?:challenged|come upon) (?P<defender>.*) \[\d+/(?P<defender_sum>\d+)\]", line)
     if m:
       postdate, attacker, attacker_sum, defender, defender_sum = m.groups()
-      itemsum[attacker] = int(attacker_sum)
-      itemsum[defender] = int(defender_sum)
+      if attacker != 'idlerpg':
+        stats[attacker]['itemsum'] = int(attacker_sum)
+      if defender != 'idlerpg':
+        stats[defender]['itemsum'] = int(defender_sum)
       continue
 
     #
@@ -228,7 +221,7 @@ with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
     m = re.match(postdate_re+r"(?P<who>.*) has changed alignment to: (.*)\.$", line)
     if m:
       postdate, who, align = m.groups()
-      alignment[who] = align.replace('neutral', 'neut')
+      stats[who]['alignment'] = align
       continue
 
     #
@@ -240,8 +233,8 @@ with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
     if m:
       postdate, who, days, hours, mins, secs = m.groups()
       duration = convert_to_duration(days, hours, mins, secs)
-      timeleft[who] += duration
-      alignment[who] = 'evil'
+      stats[who]['timeleft'] += duration
+      stats[who]['alignment'] = 'evil'
 
     # X and Y have not let the iniquities of evil men.*them.  \d+% of their time
     m = re.match(postdate_re+r'(.*?) and (.*?) have not let the iniquities of evil men.*them. (\d+)% of their time is removed from their clocks', line)
@@ -250,8 +243,8 @@ with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
       post_epoch = convert_to_epoch(postdate)
       adjust_timeleft_percentage(who1, post_epoch, int(percentage))
       adjust_timeleft_percentage(who2, post_epoch, int(percentage))
-      alignment[who1] = 'good'
-      alignment[who2] = 'good'
+      stats[who1]['alignment'] = 'good'
+      stats[who2]['alignment'] = 'good'
 
     # I, J, and K [.*] have team battled.* and (won|lost)!
     m = re.match(postdate_re+r'(.*?)\[.*?have team battled .*? and (won|lost)! (\d+) days?, (\d{2}):(\d{2}):(\d{2})', line)
@@ -262,8 +255,7 @@ with open('/home/newren/.xchat2/xchatlogs/Palantir-#idlerpg.log') as f:
       duration = convert_to_duration(days, hours, mins, secs)
       sign = -1 if (result == 'won') else 1
       for who in members:
-        if who in timeleft:
-          timeleft[who] += sign*duration
+        stats[who]['timeleft'] += sign*duration
 
 
 def time_format(seconds):
@@ -297,7 +289,7 @@ def quest_info(started, time_left, quest_times):
 
 def battle_burn(who):
   # FIXME: Really ought to handle being hit by critical strikes from others too
-  oncount = sum([1 for x in online if online[x]])
+  oncount = sum([1 for x in stats if stats[x]['online']])
   odds_fight_per_day = 24.0/oncount  # every hour, 1.0 selected to start fight
   odds_fight_per_day += 1.5/oncount  # 1.5ish grid battles per day
   # team battles are basically a wash; the reward is equal to the loss, so the
@@ -307,13 +299,13 @@ def battle_burn(who):
   # Technically, grid battles cannot be against the 'idlerpg' user, which
   # means I should adjust odds_fight_per_day and odds_fight_this_opp to be
   # more precise, but meh -- it won't change things that much.
-  for opp in online:
-    if opp == who or not online[opp]:
+  for opp in stats:
+    if opp == who or not stats[opp]['online']:
       continue
-    gain = max(7,level[opp]/4)
-    loss = max(7,level[opp]/7)
+    gain = max(7,stats[opp]['level']/4)
+    loss = max(7,stats[opp]['level']/7)
     odds_fight_this_opp = 1.0/oncount # oncount-1 other players, plus idlerpg
-    odds_beat_opp = itemsum[who]/(itemsum[who]+itemsum[opp]+0.0)
+    odds_beat_opp = stats[who]['itemsum']/(stats[who]['itemsum']+stats[opp]['itemsum']+0.0)
 
     change_if_fight = odds_beat_opp*gain - (1-odds_beat_opp)*loss
     diff = change_if_fight*odds_fight_this_opp*odds_fight_per_day
@@ -322,41 +314,44 @@ def battle_burn(who):
     gain = 20
     loss = 10
     odds_fight_this_opp = 1.0/oncount # oncount-1 other players, plus idlerpg
-    odds_beat_opp = itemsum[who]/(itemsum[who]+itemsum['idlerpg']+0.0)
+    odds_beat_opp = stats[who]['itemsum']/(stats[who]['itemsum']+stats['idlerpg']['itemsum']+0.0)
 
     change_if_fight = odds_beat_opp*gain - (1-odds_beat_opp)*loss
     diff = change_if_fight*odds_fight_this_opp*odds_fight_per_day
     percent_change += diff
-  return percent_change/100.0 * (timeleft[who]-43200)
+  return percent_change/100.0 * (stats[who]['timeleft']-43200)
 
 def godsend_calamity_hog_burn(who):
   # 1/8 chance per day (per online user) of calamity
   # 1/4 chance per day (per online user) of godsend
   # 9/10 chance per day calamity or godsend will change ttl by 5-12%
-  # 1/20 chance per day (per online user) of hog; which change ttl by 5-75%; 20% chance bad and 80% good
+  # 1/20 chance per day (per online user) of hog; which change ttl by
+  #      5-75%; 20% chance bad and 80% good
   percent_calamity = .9*(1.0/8)*(.05+.12)/2
   percent_godsend  = .9*(1.0/4)*(.05+.12)/2
   percent_hog      = .05*       (.05+.75)/2*(.8-.2)
-  return (percent_godsend-percent_calamity+percent_hog) * (timeleft[who]-43200)
+  overall_percentage = (percent_godsend-percent_calamity+percent_hog)
+  return overall_percentage * (stats[who]['timeleft']-43200)
 
 def alignment_burn(who):
-  if alignment[who] == 'good':
-    good_and_online_count = sum([1 for x in online if online[x] and alignment[x] == 'good'])
+  if stats[who]['alignment'] == 'good':
+    good_and_online_count = sum([1 for x in stats
+                   if stats[x]['online'] and stats[x]['alignment'] == 'good'])
     if good_and_online_count < 2:
       return 0
     percent = (.05+.12)/2
     odds = 2*(1.0/12)
-    return odds*percent*(timeleft[who]-43200)
-  elif alignment[who] == 'evil':
+    return odds*percent*(stats[who]['timeleft']-43200)
+  elif stats[who]['alignment'] == 'evil':
     percent = (.01+.05)/2
     odds = .5*1.0/8
-    return -odds*percent*(timeleft[who]-43200)
+    return -odds*percent*(stats[who]['timeleft']-43200)
   else:
     return 0
 
 def expected_ttl_burn(who): # How much time-to-level will decrease in next day
   # If they're not online, their time-to-level isn't going to decrease at all
-  if not online[who]:
+  if not stats[who]['online']:
     return 0
 
   # If user has less than a day left, the fact that we tend to burn faster more
@@ -364,7 +359,7 @@ def expected_ttl_burn(who): # How much time-to-level will decrease in next day
   # correct amount to report will depend upon what their ttl becomes after they
   # go to the next level.  For now, just pretend they'll burn at a flat rate
   # if they have less than half a day left
-  if timeleft[who] < 43200:
+  if stats[who]['timeleft'] < 43200:
     return 86400
 
   # By default, if people wait a day, a day of time-to-level burns
@@ -377,19 +372,28 @@ def expected_ttl_burn(who): # How much time-to-level will decrease in next day
 
 # Mark people as offline if they're unknown but their ttl suggests they
 # should have already levelled by now
-for who in online:
-  if online[who] is None and timeleft[who]+final_log_break < now:
-    online[who] = False
+for who in stats:
+  if stats[who]['online'] is None and \
+     stats[who]['timeleft']+final_log_break < now:
+    stats[who]['online'] = False
 
 # Print out all the information we've collected
 brkln="--- --- ---- ------------ ---- ------------ ---------"
 print "Lvl On? ISum  Time-to-Lvl Algn ExpectedBurn character"
 last = True
-for who in sorted(timeleft, key=lambda x:(online[x],timeleft[x])):
-  on = 'yes' if online[who] else ('???' if online[who] is None else 'no')
+for who in sorted(stats, key=lambda x:(stats[x]['online'],stats[x]['timeleft'])):
+  on = 'yes' if stats[who]['online'] else ('???'
+             if stats[who]['online'] is None else 'no')
   assumed_on = bool(on=='yes')
   if assumed_on ^ last:
     print brkln
     last = assumed_on
-  print('{:3d} {:3s} {:4d} {} {} {} {}'.format(level[who], on, itemsum[who], time_format(timeleft[who]), alignment[who], time_format(expected_ttl_burn(who)), who))
+  print('{:3d} {:3s} {:4d} {} {} {} {}'.format(
+           stats[who]['level'],
+           on,
+           stats[who]['itemsum'],
+           time_format(stats[who]['timeleft']),
+           stats[who]['alignment'][0:4],
+           time_format(expected_ttl_burn(who)),
+           who))
 print("Quest: "+quest_info(quest_started, quest_time_left, quest_times))
